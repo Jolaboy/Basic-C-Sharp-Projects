fbf517cad81b5459f18da2099910bc1e0abd9935
Amadou Jarju <163463239+Jolaboy@users.noreply.github.com>
2025-07-30 17:04:49 +0100

    Initial Commit -Exception Handling

---
+++ ExceptionHandling/ExceptionHandling/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace ExceptionHandling
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                // This program demonstrates exception handling by dividing two numbers.
+                Console.WriteLine("====Exception Handling Example====");
+                Console.WriteLine("Please enter a number:");
+                int numberOne = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Please enter a number to divide by the number:");
+                int numberTwo = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("====Dividing NumberOne and numberTwo===");
+                Console.WriteLine(numberOne + " divided by " + numberTwo + " is " + (numberOne / numberTwo));
+                Console.ReadLine();
+            }
+            catch (FormatException ex)
+            {
+                Console.WriteLine("Error: Please enter a valid number. " + ex.Message);
+            }
+            catch (DivideByZeroException ex)
+            {
+                Console.WriteLine("Error: Division by zero is not allowed. " + ex.Message);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                Console.ReadLine();
+            }
+            Console.ReadLine();
+        }
+    }
+}
