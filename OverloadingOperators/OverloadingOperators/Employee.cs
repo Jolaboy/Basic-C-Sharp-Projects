c9ca8100d0ccc64ac3cda807da5b87241f72fc7a
Amadou Jarju <163463239+Jolaboy@users.noreply.github.com>
2025-08-05 16:07:19 +0100

    Initial Commit - Operator Overloading

---
+++ OverloadingOperators/OverloadingOperators/Employee.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OverloadingOperators
+{
+    /// <summary>
+    /// Represents an employee with an ID, first name, and last name.
+    /// </summary>
+    public class Employee
+    {
+        // Public properties for the Employee's data.
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        /// <summary>
+        /// Overloads the equality operator (==) to compare two Employee objects.
+        /// </summary>
+        /// <param name="employee1">The first Employee object to compare.</param>
+        /// <param name="employee2">The second Employee object to compare.</param>
+        /// <returns>True if the Employee objects' Id properties are equal, otherwise false.</returns>
+        public static bool operator ==(Employee employee1, Employee employee2)
+        {
+            // Check for null values to avoid errors. If both are null, they are considered equal.
+            // If one is null and the other isn't, they are not equal.
+            if (ReferenceEquals(employee1, null) && ReferenceEquals(employee2, null))
+                return true;
+            if (ReferenceEquals(employee1, null) || ReferenceEquals(employee2, null))
+                return false;
+
+            // Compare the Id properties of the two Employee objects.
+            return employee1.Id == employee2.Id;
+        }
+
+        /// <summary>
+        /// Overloads the inequality operator (!=) to compare two Employee objects.
+        /// This is the logical inverse of the equality operator.
+        /// </summary>
+        /// <param name="employee1">The first Employee object to compare.</param>
+        /// <param name="employee2">The second Employee object to compare.</param>
+        /// <returns>True if the Employee objects' Id properties are not equal, otherwise false.</returns>
+        public static bool operator !=(Employee employee1, Employee employee2)
+        {
+            // By convention, the inequality operator should be the opposite of the equality operator.
+            return !(employee1 == employee2);
+        }
+
+        /// <summary>
+        /// Overrides the default Equals method. It's good practice to override this
+        /// whenever you overload the '==' operator.
+        /// </summary>
+        /// <param name="obj">The object to compare with the current object.</param>
+        /// <returns>True if the objects are equal based on their Id, otherwise false.</returns>
+        public override bool Equals(object obj)
+        {
+            // Check if the object is an Employee and then compare Ids.
+            if (obj is Employee otherEmployee)
+            {
+                return this.Id == otherEmployee.Id;
+            }
+            return false;
+        }
+
+        /// <summary>
+        /// Overrides the default GetHashCode method. This is also good practice
+        /// when overriding Equals().
+        /// </summary>
+        /// <returns>A hash code based on the object's Id property.</returns>
+        public override int GetHashCode()
+        {
+            // Use the Id property to generate a consistent hash code.
+            return Id.GetHashCode();
+        }
+    }
+
+}
---
+++ OverloadingOperators/OverloadingOperators/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OverloadingOperators
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // --- Instantiating Employee Objects ---
+            // Create two Employee objects with the same Id.
+            Employee employee1 = new Employee { Id = 0101, FirstName = "Amadou", LastName = "Jarju" };
+            Employee employee2 = new Employee { Id = 0101, FirstName = "Amie", LastName = "Sanneh" };
+
+            // Create a third Employee object with a different Id.
+            Employee employee3 = new Employee { Id = 0102, FirstName = "Assan", LastName = "Njie" };
+
+            // --- Testing the Overloaded == Operator ---
+            Console.WriteLine("--- Testing the '==' operator ---");
+
+            // Compare employee1 and employee2 (same Id).
+            if (employee1 == employee2)
+            {
+                Console.WriteLine($"Result: {employee1.FirstName} and {employee2.FirstName} are considered equal.");
+            }
+            else
+            {
+                Console.WriteLine($"Result: {employee1.FirstName} and {employee2.FirstName} are NOT considered equal.");
+            }
+
+            // Compare employee1 and employee3 (different Id).
+            if (employee1 == employee3)
+            {
+                Console.WriteLine($"Result: {employee1.FirstName} and {employee3.FirstName} are considered equal.");
+            }
+            else
+            {
+                Console.WriteLine($"Result: {employee1.FirstName} and {employee3.FirstName} are NOT considered equal.");
+            }
+
+            // --- Testing the Overloaded != Operator ---
+            Console.WriteLine("\n--- Testing the '!=' operator ---");
+
+            // Compare employee1 and employee2 (same Id).
+            if (employee1 != employee2)
+            {
+                Console.WriteLine($"Result: {employee1.FirstName} and {employee2.FirstName} are NOT considered equal.");
+            }
+            else
+            {
+                Console.WriteLine($"Result: {employee1.FirstName} and {employee2.FirstName} are considered equal.");
+            }
+
+            // Compare employee1 and employee3 (different Id).
+            if (employee1 != employee3)
+            {
+                Console.WriteLine($"Result: {employee1.FirstName} and {employee3.FirstName} are NOT considered equal.");
+            }
+            else
+            {
+                Console.WriteLine($"Result: {employee1.FirstName} and {employee3.FirstName} are considered equal.");
+            }
+
+            // --- Keep the console window open ---
+            Console.WriteLine("\nPress any key to exit...");
+            Console.ReadKey();
+
+        }
+    }
+}
