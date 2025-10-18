ddc84eea444a122e291e65a6dfa341bcc13d7e2d
Amadou Jarju <163463239+Jolaboy@users.noreply.github.com>
2025-10-18 03:23:46 +0100

    Initial Commit - MVC EF Web Application

---
+++ NewsletterMVCApp/NewsletterMVCApp/Controllers/AdminController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using NewsletterMVCApp.Models;
+using NewsletterMVCApp.ViewModels;
+
+namespace NewsletterMVCApp.Controllers
+{
+    public class AdminController : Controller
+    {
+        // GET: Admin
+        public ActionResult Index()
+        {
+            using (NewsletterEntities db = new NewsletterEntities())
+            {
+                var signups = db.SignUps;
+                var signupVms = new List<SignupVm>();
+                foreach (var signup in db.SignUps)
+                {
+                    var signupVm = new SignupVm();
+                    signupVm.Id = signup.id;
+                    signupVm.FirstName = signup.FirstName;
+                    signupVm.LastName = signup.LastName;
+                    signupVm.EmailAddress = signup.EmailAddress;
+                    signupVms.Add(signupVm);
+                }
+
+                return View(signupVms);
+            }
+        }
+        public ActionResult Unsubscribe(int id)
+        {
+            using (NewsletterEntities db = new NewsletterEntities())
+            {
+                var signup = db.SignUps.Find(id);
+                signup.Removed = DateTime.Now;
+                db.SaveChanges();
+                
+            }
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ NewsletterMVCApp/NewsletterMVCApp/Controllers/HomeController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.UI.WebControls.WebParts;
+using Microsoft.Data.SqlClient;
+using NewsletterMVCApp.Models; // instead of System.Data.SqlClient
+using NewsletterMVCApp.ViewModels;
+
+namespace NewsletterMVCApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult SignUp(string firstName, string lastName, string emailAddress)
+        {
+            if (string.IsNullOrEmpty(firstName) || string.IsNullOrEmpty(lastName) || string.IsNullOrEmpty(emailAddress))
+            {
+                return View("~/Views/Shared/Error.cshtml");
+            }
+            else
+            {
+                using (NewsletterEntities db = new NewsletterEntities())
+                {
+                    var signup = new SignUp();
+                    signup.FirstName = firstName;
+                    signup.LastName = lastName;
+                    signup.EmailAddress = emailAddress;
+
+                    db.SignUps.Add(signup);
+                    db.SaveChanges();
+                }
+                return View("Success");
+            }
+        }
+    }
+}
